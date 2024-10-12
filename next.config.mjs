d22e4bc261a20a1acfdd9eb7da486efcbc4d033c
ryosan-7 <30883561+ryoakira12345@users.noreply.github.com>
2024-10-12 23:50:26 +0900

    Fix webpack configuration and add Cloudinary host to image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-      };
-    }
-    return config;
+  output: "standalone",
+  images: {
+    domains: ["res.cloudinary.com"], // Cloudinary のホストを追加
   },
 };
 
